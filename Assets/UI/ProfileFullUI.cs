3cd0256683abc1300994e16c1cca2d2125713cec
Marlore <68165023+Marlore@users.noreply.github.com>
2023-12-12 15:30:57 +0300

    Godot init

--- Assets/UI/ProfileFullUI.cs
+++ Assets/UI/ProfileFullUI.cs
@@ -1,12 +1,17 @@
+using Engine.PlayerEngine;
 using Godot;
 using System;
 
 public partial class ProfileFullUI : Window
 {
-	// Called when the node enters the scene tree for the first time.
+	Label PersonName;
+	Label PersonAge;
 	public override void _Ready()
 	{
+		PersonName = (Label)this.GetNode("Control/HBoxContainer/VBoxContainer/Name");
+		PersonAge = (Label)this.GetNode("Control/HBoxContainer/VBoxContainer/Age");
 		this.Hide();
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,6 +20,9 @@ public partial class ProfileFullUI : Window
 		GD.Print(id);
 		this.Show();
 		Guid guid = Guid.Parse(id);
+		var person = PlayerInfo.CurrentCity.Population[guid];
+		PersonName.Text = person.FirstName + " " + person.SecondName;
+		PersonAge.Text = "Age:" + person.Age;
 	}
 	private void _on_close_requested()
 	{
